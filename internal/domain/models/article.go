5ade2326fac88bb137064fd959a5160f05c079f1
daria_smyr <dasha.smyr@gmail.com>
2025-03-07 00:14:02 +0300

    Fixed models

--- internal/domain/models/article.go
+++ internal/domain/models/article.go
@@ -1,23 +1,17 @@
 package models
 
-type Article struct {
-	PageID  int    `json:"pageid"`
-	NS      int    `json:"ns"`
-	Title   string `json:"title"`
-	Extract string `json:"extract"`
-}
-
-type APIResponse struct {
+type ArticleResponse struct {
 	BatchComplete string `json:"batchcomplete"`
 	Query         Query  `json:"query"`
 }
 
 type Query struct {
-	Normalized []NormalizedEntry  `json:"normalized"`
-	Pages      map[string]Article `json:"pages"`
+	Pages map[string]Article `json:"pages"`
 }
 
-type NormalizedEntry struct {
-	From string `json:"from"`
-	To   string `json:"to"`
+type Article struct {
+	PageID  int    `json:"pageid"`
+	NS      int    `json:"ns"`
+	Title   string `json:"title"`
+	Extract string `json:"extract"`
 }
