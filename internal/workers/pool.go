91efb45e8958ca94f012776d261351ae8ff6e985
daria_smyr <dasha.smyr@gmail.com>
2025-03-09 23:48:20 +0300

    Feat: made job channel non buffer, removed results chan and worker num limit. Added logging errors to file

--- internal/workers/pool.go
+++ internal/workers/pool.go
@@ -2,83 +2,112 @@ package workers
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"os"
+	"runtime"
 	"sync"
+	"sync/atomic"
 )
 
 type WorkerPool struct {
-	workersCount  int
 	jobs          chan Job
-	results       chan Result
 	Done          chan struct{}
-	activeWorkers chan int
+	activeWorkers int32
+	logFile       *os.File
+	logMutex      sync.Mutex
 }
 
-func (wp *WorkerPool) GenerateFrom(jobsBulk []Job) {
-	for i := range jobsBulk {
-		wp.jobs <- jobsBulk[i]
-	}
-
-	close(wp.jobs)
+type JobError struct {
+	JobDescription JobDescriptor `json:"job_description"`
+	Error          string        `json:"error"`
 }
 
 func (wp *WorkerPool) AddJob(job *Job) {
 	wp.jobs <- *job
 }
 
-func (wp *WorkerPool) ActiveWorkersCount() int {
-	return len(wp.activeWorkers)
+func (wp *WorkerPool) ActiveWorkersCount() int32 {
+	return atomic.LoadInt32(&wp.activeWorkers)
 }
 
 func (wp *WorkerPool) JobChannelCount() int {
 	return len(wp.jobs)
 }
 
+func (wp *WorkerPool) MemoryUsage() uint64 {
+	var memStats runtime.MemStats
+	runtime.ReadMemStats(&memStats)
+	return memStats.Alloc
+}
+
 func (wp *WorkerPool) Run(ctx context.Context) {
 	var wg sync.WaitGroup
 
-	for i := 0; i < wp.workersCount; i++ {
-		wg.Add(1)
-		go worker(ctx, &wg, wp.jobs, wp.results, wp.activeWorkers)
+	var err error
+	wp.logFile, err = os.Create("worker_errors.json")
+	if err != nil {
+		fmt.Printf("Error opening log file: %v\n", err)
+		return
 	}
+
+	go func() {
+		for {
+			select {
+			case job, ok := <-wp.jobs:
+				if !ok {
+					fmt.Println("No more jobs to process, closing worker pool")
+					close(wp.Done)
+					return
+				}
+
+				wg.Add(1)
+				go worker(ctx, &wg, job, wp)
+			}
+		}
+	}()
 
 	wg.Wait()
-	close(wp.Done)
-	close(wp.results)
 }
 
-func worker(ctx context.Context, wg *sync.WaitGroup, jobs <-chan Job, results chan<- Result, activeWorkers chan int) {
+func (wp *WorkerPool) CloseLogFile() error {
+	err := wp.logFile.Close()
+	if err != nil {
+		return fmt.Errorf("Error closing log file: %v", err)
+	}
+	return nil
+}
+
+func worker(ctx context.Context, wg *sync.WaitGroup, job Job, wp *WorkerPool) {
 	defer wg.Done()
 
-	activeWorkers <- 1
+	atomic.AddInt32(&wp.activeWorkers, 1)
+	defer atomic.AddInt32(&wp.activeWorkers, -1)
 
-	for {
-		select {
-		case job, ok := <-jobs:
-			if !ok {
-				<-activeWorkers
-				return
+	select {
+	case <-ctx.Done():
+		fmt.Printf("Worker cancelled: %v\n", ctx.Err())
+		return
+	default:
+		result := job.execute(ctx)
+		if result.Err != nil {
+			jobErr := JobError{
+				JobDescription: job.Description,
+				Error:          result.Err.Error(),
 			}
-			result := job.execute(ctx)
-			results <- result
-		case <-ctx.Done():
-			fmt.Printf("cancelled worker. Error detail: %v/n", ctx.Err())
-			results <- Result{
-				Err: ctx.Err(),
+			wp.logMutex.Lock()
+			defer wp.logMutex.Unlock()
+			encoder := json.NewEncoder(wp.logFile)
+			if err := encoder.Encode(jobErr); err != nil {
+				fmt.Printf("Failed to write JSON log: %v\n", err)
 			}
-			<-activeWorkers
-			return
 		}
-
 	}
 }
 
-func New(wcount int, jobcount int) WorkerPool {
+func New() WorkerPool {
 	return WorkerPool{
-		workersCount:  wcount,
-		jobs:          make(chan Job, jobcount),
-		results:       make(chan Result, jobcount),
-		activeWorkers: make(chan int, wcount),
-		Done:          make(chan struct{}),
+		jobs: make(chan Job),
+		Done: make(chan struct{}),
 	}
 }
