80c387f43621cefd82ef6b6ef0e9c302ecfeef6c
darya.smyr <darya.smyr@lamoda.ru>
2026-03-20 20:53:31 +0300

    refactor: moved filter registry to pgf/fts

--- pkg/fts/registry.go
+++ pkg/fts/registry.go
@@ -6,10 +6,13 @@ import (
 )
 
 type IndexFactory func() (Index, error)
+type FilterFactory func() (Filter, error)
 
 var (
-	registryMu sync.RWMutex
-	registry   = make(map[string]IndexFactory)
+	indexRegistryMu  sync.RWMutex
+	indexRegistry    = make(map[string]IndexFactory)
+	filterRegistryMu sync.RWMutex
+	filterRegistry   = make(map[string]FilterFactory)
 )
 
 func RegisterIndex(name string, factory IndexFactory) error {
@@ -20,23 +23,53 @@ func RegisterIndex(name string, factory IndexFactory) error {
 		return fmt.Errorf("fts: register index: nil factory")
 	}
 
-	registryMu.Lock()
-	defer registryMu.Unlock()
+	indexRegistryMu.Lock()
+	defer indexRegistryMu.Unlock()
 
-	if _, exists := registry[name]; exists {
+	if _, exists := indexRegistry[name]; exists {
 		return fmt.Errorf("fts: register index: duplicate name %q", name)
 	}
 
-	registry[name] = factory
+	indexRegistry[name] = factory
 	return nil
 }
 
 func NewIndex(name string) (Index, error) {
-	registryMu.RLock()
-	factory, ok := registry[name]
-	registryMu.RUnlock()
+	indexRegistryMu.RLock()
+	factory, ok := indexRegistry[name]
+	indexRegistryMu.RUnlock()
 	if !ok {
 		return nil, fmt.Errorf("fts: unknown index %q", name)
+	}
+
+	return factory()
+}
+
+func RegisterFilter(name string, factory FilterFactory) error {
+	if name == "" {
+		return fmt.Errorf("filter: register: empty name")
+	}
+	if factory == nil {
+		return fmt.Errorf("filter: register: nil factory")
+	}
+
+	filterRegistryMu.Lock()
+	defer filterRegistryMu.Unlock()
+
+	if _, exists := filterRegistry[name]; exists {
+		return fmt.Errorf("filter: register: duplicate name %q", name)
+	}
+
+	filterRegistry[name] = factory
+	return nil
+}
+
+func NewFilter(name string) (Filter, error) {
+	filterRegistryMu.RLock()
+	factory, ok := filterRegistry[name]
+	filterRegistryMu.RUnlock()
+	if !ok {
+		return nil, fmt.Errorf("filter: unknown %q", name)
 	}
 
 	return factory()
